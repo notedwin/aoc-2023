ad0c7e81ebf4e9b4249eae29d3aeab74f948a593
Edwin Zamudio <zamudio.e13@gmail.com>
2023-12-04 23:28:50 -0600

    add a setup script and move files into folders

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if the number of arguments is correct
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <day_number>"
+    exit 1
+fi
+
+name="day$1"
+echo $name
+
+mkdir $name 
+
+touch "$name/$name.py"
+touch "$name/${name}_test.txt"
+touch "$name/$name.txt"
+
+echo "'$name' setup successfully."
